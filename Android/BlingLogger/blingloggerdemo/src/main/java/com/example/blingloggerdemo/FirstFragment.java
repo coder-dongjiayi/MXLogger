9cef0d545d0fee7d5bc7023c888e47d4a4087377
董家祎 <>
2022-03-23 11:37:15 +0800

    完成Android 工程框架基本搭建

---
+++ Android/BlingLogger/blingloggerdemo/src/main/java/com/example/blingloggerdemo/FirstFragment.java
@@ -0,0 +1,54 @@
+package com.example.blingloggerdemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.blingabc.blinglogger.NativeLib;
+import com.example.blingloggerdemo.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NativeLib nativeLib = new NativeLib();
+                String t =  nativeLib.stringFromJNI();
+
+                Log.d("",t);
+
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
