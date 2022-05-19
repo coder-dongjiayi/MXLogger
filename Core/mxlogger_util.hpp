2b8f8465e09a52b8f9cb69e58f332e7ce7958e58
董家祎 <>
2022-05-19 20:17:44 +0800

    no message

--- Core/mxlogger_util.hpp
+++ Core/mxlogger_util.hpp
@@ -19,10 +19,7 @@ class mxlogger_util{
 public:
     static long select_log_form_path(const char* path,std::vector<std::string> *destination);
     
-    
-    /// 查询目录下所有的日志文件 按照时间倒xu排列
-    /// @param dir 目录
-    /// @param result 0 查询成功
+
     static int select_logfiles_dir(const char* dir,std::vector<std::map<std::string, std::string>> *destination);
 };
 
