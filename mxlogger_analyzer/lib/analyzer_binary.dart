1e0d9e10395c0673c4e7b7a70469b1480de8f900
董家祎 <>
2022-07-05 18:54:06 +0800

    no message

--- mxlogger_analyzer/lib/analyzer_binary.dart
+++ mxlogger_analyzer/lib/analyzer_binary.dart
@@ -6,26 +6,31 @@ import 'log_serialize.dart';
 
 class AnalyzerBinary{
 
-  static void decode({required Uint8List binaryData}){
-    int sizeof_uint32t = 4;
+  static void decode({required Uint8List binaryData, String? cryptKey,String? iv}){
+    int sizeofUint32t = 4;
+
+    int offsetLength =  sizeofUint32t;
+
+
     Uint8List  actualUint8List = binaryData.sublist(0,4);
 
     ByteData actualData =  actualUint8List.buffer.asByteData();
     int totalSize =  actualData.getUint32(0,Endian.little);
-    int begin = 4;
+    int begin = offsetLength;
     while(begin <= totalSize){
 
-      Uint8List itemData = binaryData.sublist(begin,begin + sizeof_uint32t);
+      Uint8List itemData = binaryData.sublist(begin,begin + sizeofUint32t);
       int itemSize = itemData.buffer.asByteData().getUint32(0,Endian.little);
 
-      int start = begin + sizeof_uint32t;
+      int start = begin + sizeofUint32t;
 
       Uint8List buffer = binaryData.sublist(start,start+itemSize);
       LogSerialize logSerialize =   LogSerialize(buffer);
+
       print("name:${logSerialize.name}");
       print("name:${logSerialize.msg}");
 
-      begin = begin + sizeof_uint32t + itemSize;
+      begin = begin + sizeofUint32t + itemSize;
 
     }
   }
